94416d2a142621e79d6e1fe0ce5833ddd5565e95
Sai Spandana Gali <60799194+span97@users.noreply.github.com>
2020-12-28 14:37:32 -0600

    Add files via upload

---
+++ stock.py
@@ -0,0 +1,19 @@
+import yfinance as yf
+import streamlit as st
+import pandas as pd
+
+st.write("""
+# Simple stock price app
+
+Shown in google!
+
+""")
+
+tickerSymbol ='GOOGL'
+
+tickerData = yf.Ticker(tickerSymbol)
+
+tickerDf = tickerData.history(period='1d', start='2010-5-31',  end='2020-5-31')
+
+st.line_chart(tickerDf.Close)
+st.line_chart(tickerDf.Volume)
